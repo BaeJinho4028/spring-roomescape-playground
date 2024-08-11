50498bef44bdadf9e5106ac393aec8f092362702
Baejinho4028 <onionring4028@naver.com>
2024-08-11 19:28:04 +0900

    feat: infra study 용 코드 수정

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,19 +1,31 @@
 package roomescape.domain;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
+@Table(name = "reservation")
 public class Reservation {
 
-    private final Long id;
+    @Id
+    @Column(name = "id")
+    private Long id;
 
-    private final String name;
+    @Column(name = "name")
+    private String name;
 
-    private final LocalDate date;
+    @Column(name = "date")
+    private LocalDate date;
 
-    private final Time time;
+    @Column(name = "time")
+    private Time time;
 }
--- src/main/java/roomescape/domain/Time.java
+++ src/main/java/roomescape/domain/Time.java
@@ -2,14 +2,23 @@ package roomescape.domain;
 
 import java.time.LocalTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
+@Table(name = "time")
 public class Time {
 
-    private final Long id;
+    @Id
+    @Column(name = "id")
+    private Long id;
 
-    private final LocalTime time;
+    @Column(name = "time")
+    private LocalTime time;
 }
